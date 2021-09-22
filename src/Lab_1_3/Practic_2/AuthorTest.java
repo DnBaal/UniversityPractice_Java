1202241abf9cab58a4a2ccd52e18aaaedbc5d615
DnBaal <dantheassassin2000@gmail.com>
2021-09-22 22:08:40 +0300

    Updated

--- src/Lab_1_3/Practic_2/AuthorTest.java
+++ src/Lab_1_3/Practic_2/AuthorTest.java
@@ -1,4 +1,16 @@
 package Lab_1_3.Practic_2;
 
 public class AuthorTest {
+
+    public static void main(String[] args) {
+
+        Author author = new Author("Чехов", "chehov@mail.ru", 'M');
+
+        System.out.println(author);
+
+        System.out.println("Changing email..");
+        author.setEmail("new-chehov@mail.ru");
+        System.out.println(author);
+
+    }
 }
--- src/Lab_1_3/Practic_2/Main.java
+++
@@ -1,9 +0,0 @@
-package Lab_1_3.Practic_2;
-
-public class Main {
-    public static void main(String[] args) {
-
-
-    }
-
-}
